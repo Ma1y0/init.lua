ba0a453eecdaf6411a55ca2199454dd830f29c96
Ma1y0 <matyas.barr@gmail.com>
2024-05-13 21:25:02 +0200

    Terminal

--- lua/ma1y0/core/init.lua
+++ lua/ma1y0/core/init.lua
@@ -1,3 +1,4 @@
 require("ma1y0.core.keymaps")
 require("ma1y0.core.options")
 require("ma1y0.core.autocmds")
+require("ma1y0.core.terminal")
---
+++ lua/ma1y0/core/terminal.lua
@@ -0,0 +1,33 @@
+local set = vim.opt_local
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", {}),
+	callback = function()
+		set.number = false
+		set.relativenumber = false
+		set.scrolloff = 0
+		vim.opt_local.spell = false -- Disable spell checking
+		vim.cmd("startinsert") -- Starts in insert mode
+	end,
+})
+
+-- Easily hit escape in terminal mode.
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- Opens terminal at the bottom
+vim.keymap.set("n", "<leader>tb", function()
+	vim.cmd.new()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 12)
+	vim.wo.winfixheight = true
+	vim.cmd.term()
+end, { desc = "Opens new terminal at the bottom" })
+
+-- Opens terminal at the right side
+vim.keymap.set("n", "<leader>tr", function()
+	vim.cmd.vnew() -- Open a new vertical split
+	vim.cmd.wincmd("l") -- Move cursor to the new split
+	vim.api.nvim_win_set_width(0, 40) -- Set the width of the new split
+	vim.wo.winfixwidth = true -- Fix the width of the new split
+	vim.cmd.term()
+end, { desc = "Opens new terminal on the right" })
